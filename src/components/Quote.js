ff68c492d820d40a57a7f39dc3477e9024e2d801
Kelvin <ngotho.kelvin@yahoo.com>
2023-03-25 12:58:11 +0300

    Remmove Identation spaces

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,49 +1,49 @@
 import React, { useState, useEffect } from "react";
 
 const Quote = () => {
-    const [quote, setQuote] = useState({});
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const [quote, setQuote] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchQuote = async () => {
-            setIsLoading(true);
+  useEffect(() => {
+    const fetchQuote = async () => {
+      setIsLoading(true);
 
-            try {
-                const response = await fetch(
-                    "https://api.api-ninjas.com/v1/quotes?category=happiness",
-                    {
-                        headers: {
-                            "X-Api-Key": "arjAxhHIzmx1T4/2+TN60A==qHFdGV86rvZbZX1a",
-                        },
-                    }
-                );
-                const data = await response.json();
-                setQuote(data[0]);
-                setIsLoading(false);
-            } catch (error) {
-                setIsLoading(false);
-                setError(error);
-            }
-        };
+      try {
+        const response = await fetch(
+          "https://api.api-ninjas.com/v1/quotes?category=happiness",
+          {
+            headers: {
+              "X-Api-Key": "arjAxhHIzmx1T4/2+TN60A==qHFdGV86rvZbZX1a",
+            },
+          }
+        );
+        const data = await response.json();
+        setQuote(data[0]);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        setError(error);
+      }
+    };
 
-        fetchQuote();
-    }, []);
+    fetchQuote();
+  }, []);
 
-    if (isLoading) {
-        return <p>Loading...</p>;
-    }
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
-    if (error) {
-        return <p>Sorry, there was an error fetching the quote.</p>;
-    }
+  if (error) {
+    return <p>Sorry, there was an error fetching the quote.</p>;
+  }
 
-    return (
-        <div>
-            <h2>{quote.author}</h2>
-            <p>{quote.quote}</p>
-        </div>
-    );
+  return (
+    <div>
+      <h2>{quote.author}</h2>
+      <p>{quote.quote}</p>
+    </div>
+  );
 };
 
 export default Quote;
